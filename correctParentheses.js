adcfdf65d0de14e72afabf794fb0eca878a3e7d4
beom <bdh3659@naver.com>
2025-01-02 22:14:11 +0900

    fix: 올바른 괄호 정답 수정

--- correctParentheses.js
+++ correctParentheses.js
@@ -1,10 +1,9 @@
 function solution(s) {
   var answer = true
-
   let stack = []
 
   for (let i = 0; i < s.length; i++) {
-    if (stack.length > 0 && stack[stack.length - 1] == "(" && s[i] == ")") {
+    if (stack.length > 0 && stack[stack.length - 1] === "(" && s[i] === ")") {
       stack.pop()
     } else {
       stack.push(s[i])
@@ -15,3 +14,5 @@ function solution(s) {
   }
   return answer
 }
+
+// 바로 count로 처리해도 되는 부분
