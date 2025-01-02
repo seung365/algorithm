779e7de2053d0d737bb0c379c50d2565280b19a1
beom <bdh3659@naver.com>
2025-01-02 22:03:00 +0900

    solve: 올바른 괄호

---
+++ correct parentheses.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  var answer = true
+
+  let stack = []
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length > 0 && stack[stack.length - 1] == "(" && s[i] == ")") {
+      stack.pop()
+    } else {
+      stack.push(s[i])
+    }
+  }
+  if (stack.length > 0) {
+    answer = false
+  }
+  return answer
+}
